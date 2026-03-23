cc94b0bad780adf1388c1c21392a6f61edcd3e02
Vibesdk <vibesdk-bot@cloudflare.com>
2026-03-23 20:43:26 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add nanoid@^5.0.0"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
